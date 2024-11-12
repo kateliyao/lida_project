b140693f908da450d36d24045ef77c62509804a3
kate009 <kate009@ny.corp>
2024-11-12 10:28:44 +0800

    20241112_kate_submit_form內容調整

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS
+import os
+import pdfkit
+from flask import send_file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173"])
@@ -17,6 +21,7 @@ mysql = MySQL(app)
 def get_db_connection():
     """创建并返回数据库连接"""
     return mysql.connection.cursor()
+
 
 #登入驗證
 @app.route('/api/login', methods=['POST'])
@@ -57,9 +62,9 @@ def get_company_name():
         except Exception as e:
             return jsonify({'message': str(e)}), 500
         finally:
-            cursor.close()  # 关闭游标
+            cursor.close()
     else:
-        return jsonify({'message': '客户编号不能为空'}), 400
+        return jsonify({'message': '客户編號不能为空'}), 400
 
 # 獲取員工資訊
 @app.route('/api/getStaffInfo', methods=['GET'])
@@ -77,7 +82,7 @@ def get_staff_info():
     except Exception as e:
         return jsonify({'message': str(e)}), 500
 
-# 新增一個處理 formData 提交的路由
+# 表單資料提交
 @app.route('/api/submitForm', methods=['POST'])
 def submit_form():
     data = request.get_json()
@@ -135,6 +140,69 @@ def submit_form():
 
         # 提交事务
         mysql.connection.commit()
+
+        #千分位、負數紅字
+        revenue = float(revenue)
+        cost = float(cost)
+        expense = float(expense)
+        profit = float(profit)
+        nonrevenue = float(nonrevenue)
+        noncost = float(noncost)
+        income = float(income)
+        netincome = float(netincome)
+        voucherNumber = income - netincome
+        extracost = float(extracost)
+        extraexpense = float(extraexpense)
+
+
+        formatted_revenue = f"({abs(revenue):,.0f})" if revenue < 0 else f"{revenue:,.0f}"
+        formatted_cost = f"({abs(cost):,.0f})" if cost < 0 else f"{cost:,.0f}"
+        formatted_expense = f"({abs(expense):,.0f})" if expense < 0 else f"{expense:,.0f}"
+        formatted_profit = f"({abs(profit):,.0f})" if profit < 0 else f"{profit:,.0f}"
+        formatted_nonrevenue = f"({abs(nonrevenue):,.0f})" if nonrevenue < 0 else f"{nonrevenue:,.0f}"
+        formatted_noncost = f"({abs(noncost):,.0f})" if noncost < 0 else f"{noncost:,.0f}"
+        formatted_income = f"({abs(income):,.0f})" if income < 0 else f"{income:,.0f}"
+        formatted_netincome = f"({abs(netincome):,.0f})" if netincome < 0 else f"{netincome:,.0f}"
+        formatted_voucherNumber = f"({abs(voucherNumber):,.0f})" if voucherNumber < 0 else f"{voucherNumber:,.0f}"
+        formatted_extracost = f"({abs(extracost):,.0f})" if extracost < 0 else f"{extracost:,.0f}"
+        formatted_extraexpense = f"({abs(extraexpense):,.0f})" if extraexpense < 0 else f"{extraexpense:,.0f}"
+
+        # 格式化比例只顯示小數點後兩位
+        costPercent=round(costPercent, 2)
+        expensePercent=round(expensePercent, 2)
+        profitPercent=round(profitPercent, 2)
+        nonrevenuePercent=round(nonrevenuePercent, 2)
+        noncostPercent=round(noncostPercent, 2)
+        incomePercent=round(incomePercent, 2)
+
+
+
+
+        # 根據isChecked選擇不同的模板
+        # template_name = 'pdf_template1.html' if isChecked == 'Y' else 'pdf_template2.html'
+        html_content = render_template('pdf_template.html', formId=formId,companyName=companyName,year1=year1,month1=month1
+        ,revenue=revenue,formatted_revenue=formatted_revenue,cost=cost,formatted_cost=formatted_cost,expense=expense,formatted_expense=formatted_expense
+        ,profit=profit,formatted_profit=formatted_profit,nonrevenue=nonrevenue,formatted_nonrevenue=formatted_nonrevenue,noncost=noncost
+        ,formatted_noncost=formatted_noncost,income=income,formatted_income=formatted_income,costPercent=costPercent,expensePercent=expensePercent
+        ,profitPercent=profitPercent,nonrevenuePercent=nonrevenuePercent,noncostPercent=noncostPercent,incomePercent=incomePercent,netincome=netincome
+        ,formatted_netincome=formatted_netincome,voucherNumber=voucherNumber,formatted_voucherNumber=formatted_voucherNumber,extracost=extracost
+        ,formatted_extracost=formatted_extracost,extraexpense=extraexpense,formatted_extraexpense=formatted_extraexpense,note=note,selectedStaff=selectedStaff
+        ,year=year,month=month,date=date)
+
+        # 使用 pdfkit 生成 PDF
+        pdf_path = os.path.join(os.getcwd(), '憑證統計表.pdf')
+
+        options = {
+            'encoding': 'UTF-8', #可以解決中文亂碼問題
+            'no-outline': None,  # 禁用文檔輪廓
+            'quiet': None         # 禁用日志
+        }
+        # 將 HTML 文件轉換為 PDF
+        pdfkit.from_string(html_content, pdf_path, options=options)
+
+        # 返回 PDF 文件
+        return send_file(pdf_path, as_attachment=True, download_name="憑證統計表.pdf", mimetype='application/pdf')
+
 
         return jsonify({'success': True, 'message': '表單資料提交成功'}), 201
 
